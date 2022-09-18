c7aaa979ea23c4b84ac8dc359b0912b4aa49a53a
Esteban Zepeda <estzepeda@outlook.com>
2022-09-17 23:05:35 -0500

    Create expense modal (Finished)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -116,6 +116,14 @@ export default function Navbar() {
     setCreateExpense(false);
   }
 
+  const modalStyle = {
+    overlay: {
+      position: 'fixed',
+      inset: '0px',
+      backgroundColor: 'rgba(0,0,0,0.25)'
+    }
+  }
+
   return (
     <nav className="nav">
       <NavLink to='/'>
@@ -134,59 +142,101 @@ export default function Navbar() {
         <button onClick={connect}>Connect</button>
       )}
       {createExpense ? (
-        <Modal isOpen={createExpense} onRequestClose={() => setCreateExpense(false)}>
-          <h2>Create a shared expense</h2>
-          <p>Name</p>
-          <input type="text" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} />
-          <p>Description</p>
-          <input type="text" value={expenseDescription} onChange={(e) => setExpenseDescription(e.target.value)} />
-          <p>Amount</p>
-          <input type="number" value={expenseAmount} onChange={(e) => setExpenseAmount(e.target.value)} />
-          <p>Token</p>
-          <select name="token" id="token" value={expenseToken} onChange={(e) => setExpenseToken(e.target.value)}>
-            <option value="EVMOS">EVMOS</option>
-            <option value="ATOM">ATOM</option>
-            <option value="USDC">USDC</option>
-          </select>
-          <p>Category</p>
-          <select name="category" id="category" value={expenseCategory} onChange={(e) => setExpenseCategory(e.target.value)}>
-            <option value="">Select a category</option>
-            <option value="/food.jpeg">Food</option>
-            <option value="/transport.jpeg">Transport</option>
-            <option value="/entertainment.jpeg">Entertainment</option>
-            <option value="/other.jpeg">Other</option>
-          </select>
-          <p>Active until</p>
-          <input type="date" value={expenseDate} onChange={(e) => setExpenseDate(e.target.value)} />
-          <p>Debtors</p>
-          <p>Debtor</p>
-          <p>Amount</p>
-          {expenseDebtors.map((x, i) => {
-            return (
-              <div className="">
-                <input
-                  name="address"
-                  placeholder="0x1234...5678"
-                  value={x.address}
-                  onChange={e => handleInputChange(e, i)}
-                />
-                <input
-                  className=""
-                  name="amount"
-                  placeholder="$"
-                  value={x.amount}
-                  onChange={e => handleInputChange(e, i)}
-                />
-                <div className="">
-                  {expenseDebtors.length !== 1 && <button
-                    className="mr10"
-                    onClick={() => handleRemoveClick(i)}>Remove</button>}
-                  {expenseDebtors.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+        <Modal isOpen={createExpense} onRequestClose={() => setCreateExpense(false)} className="modal" style={modalStyle}>
+          <div className="modal-content container">
+            <button onClick={() => setCreateExpense(false)} className="close"><img src="./img/icon/close.svg" alt="close button icon" /></button>
+            <div className="top row">
+              <h2 className="title">Create a shared expense</h2>
+            </div>
+            <div className="form-container">
+              <div className="form row">
+                <div className="form-col col-12">
+                  <label for="formName">Name</label>
+                  <input id="formName" type="text" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} />
                 </div>
               </div>
-            );
-          })}
-          <button onClick={sendExpense}>Create</button>
+              {/*<div className="form row">
+                <div className="form-col col-12">
+                  <label for="formDescription">Description</label>
+                  <input id="formDescription" type="text" value={expenseDescription} onChange={(e) => setExpenseDescription(e.target.value)} />
+                </div>
+              </div>*/}
+              <div className="form row">
+                <div className="form-col col-6">
+                  <label for="token">Token</label>
+                  <select name="token" id="token" value={expenseToken} onChange={(e) => setExpenseToken(e.target.value)}>
+                    <option value="EVMOS">EVMOS</option>
+                    <option value="ATOM">ATOM</option>
+                    <option value="USDC">USDC</option>
+                  </select>
+                </div>
+                <div className="form-col col-6">
+                  <label for="formTotal">Total</label>
+                  <input id="formTotal" type="number" value={expenseAmount} onChange={(e) => setExpenseAmount(e.target.value)} />
+                </div>
+              </div>
+              <div className="form row">
+                <div className="form-col col-12">
+                  <label for="category">Category</label>
+                  <select name="category" id="category" value={expenseCategory} onChange={(e) => setExpenseCategory(e.target.value)}>
+                    <option value="">Select a category</option>
+                    <option value="/food.jpeg">Food</option>
+                    <option value="/transport.jpeg">Transport</option>
+                    <option value="/entertainment.jpeg">Entertainment</option>
+                    <option value="/other.jpeg">Other</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form row">
+                <div className="form-col col-12">
+                  <label for="formDescription">Description</label>
+                  <textarea id="formDescription" type="text" value={expenseDescription} onChange={(e) => setExpenseDescription(e.target.value)} rows="3" placeholder="The description comes here..." />
+                </div>
+              </div>
+              <div className="form row">
+                <div className="form-col col-12">
+                  <label for="formDate">Active until</label>
+                  <input id="formDate" type="date" value={expenseDate} onChange={(e) => setExpenseDate(e.target.value)} />
+                </div>
+              </div>
+            </div>
+            <div className="top row">
+              <h2 className="title">Debtors</h2>
+            </div>
+            <div className="form-container">
+              <div className="form row">
+                <div className="form-col col-6">
+                  <label>Debtor</label>
+                </div>
+                <div className="form-col col-6">
+                  <label>Amount</label>
+                </div>
+              </div>
+              {expenseDebtors.map((x, i) => {
+                return (
+                  <div className="form row">
+                    <div className="form-col col-6">
+                      <input name="address" placeholder="0x1234...5678" value={x.address} onChange={e => handleInputChange(e, i)} />
+                    </div>
+                    <div className="form-col col-5">
+                      <input className="" name="amount" placeholder="$" value={x.amount} onChange={e => handleInputChange(e, i)} />
+                    </div>
+                    <div className="form-col col-1">
+                      {expenseDebtors.length !== 1 && <button className="remove-btn mr10" onClick={() => handleRemoveClick(i)}><img src="./img/icon/close.svg" alt="remove debtor button icon" /></button>}
+                    </div>
+                    <div className="form-col col-12">
+                      {expenseDebtors.length - 1 === i && <button className="add-btn" onClick={handleAddClick}>Add new debtor</button>}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="bottom row">
+              <div className="bottom-content">
+                <button className="create-btn" onClick={sendExpense}>Create expense</button>
+              </div>
+            </div>
+          </div>
         </Modal>
       ) : null}
     </nav>
